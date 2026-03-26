16a3bc9fcdf238ae9c8538f731c0e0d265192f58
mkg203 <kailashguru129@gmail.com>
2026-03-26 16:20:52 +0530

    pkgs

--- nvim/init.lua
+++ nvim/init.lua
@@ -26,9 +26,9 @@ vim.opt.showmode = false
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
 end)
 
+vim.opt.clipboard = 'unnamedplus'
 -- Enable break indent
 vim.opt.breakindent = true
 
